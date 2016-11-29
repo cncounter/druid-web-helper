d168c0770cfaa951a204f2e3415ae2bf7b628155
renfufei <renfufei@qq.com>
2016-11-29 18:06:23 +0800

    状态合并-API接口

---
+++ src/main/java/com/cncounter/druid/web/handler/DruidStatMergeHandlerAPI.java
@@ -0,0 +1,23 @@
+package com.cncounter.druid.web.handler;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+
+/**
+ * 状态合并
+ */
+public interface DruidStatMergeHandlerAPI {
+
+    /**
+     * 可以处理的URI
+     * @return
+     */
+    public String canProcessUri();
+    /**
+     * 合并结果
+     * @param multiResponse 请求得到的多个结果
+     * @return 合并后的单个结果
+     */
+    public JSONObject merge(List<JSONObject> multiResponse);
+}
